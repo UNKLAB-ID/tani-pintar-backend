9146f51e45bbf26a170a9feca1a6cb3eb74b8166
arter.tendean.07 <arter.tendean.07@gmail.com>
2025-01-02 17:03:14 +0000

    Update admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,14 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "full_name",
+        "email",
+        "phone_number",
+        "profile_type",
+        "id_card_validation_status",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = ("created_at", "updated_at")
+    search_fields = ("full_name", "email", "phone_number")
