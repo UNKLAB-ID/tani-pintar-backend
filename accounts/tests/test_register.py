2cff26d9945ba8dfa98c544be18e123fa9281f51
arter.tendean.07 <arter.tendean.07@gmail.com>
2025-01-03 06:01:47 +0000

    Update testing

--- accounts/tests/test_register.py
+++ accounts/tests/test_register.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from accounts.models import Profile
+from accounts.models import VerificationCode
 from core.users.models import User
 
 
@@ -55,3 +56,43 @@ class RegisterTests(TestCase):
         response = self.client.post(self.url, data, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.json().get("email")[0] == "Email already exists"
+
+    def test_register_confirmation(self):
+        id_card = SimpleUploadedFile(
+            "test_id_card.jpg",
+            b"dummy image content",
+            content_type="image/jpeg",
+        )
+
+        data = {
+            "name": "Arter Tendean",
+            "email": "arter@animemoe.us",
+            "phone_number": "123456789",
+            "id_card_file": id_card,
+            "password": "Password123!",
+        }
+
+        response = self.client.post(self.url, data, format="json")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert (
+            User.objects.filter(username=data.get("email")).exists() is True
+        ), "User should be created"
+        assert (
+            Profile.objects.filter(email=data.get("email")).exists() is True
+        ), "Profile should be created"
+
+        verification_code = VerificationCode.objects.filter(
+            user__username=data.get("email"),
+        ).last()
+        assert verification_code is not None, "Verification code should be created"
+
+        data = {"email": "arter@animemoe.us", "code": verification_code.code}
+        response = self.client.post(
+            reverse("accounts:confirm-registration"),
+            data,
+            format="json",
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        user = User.objects.get(username=data.get("email"))
+        assert user.is_active is True, "User should be activated"
