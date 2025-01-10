f46f203200228fcc96e021eab47a5c27fb629c03
arter.tendean.07 <arter.tendean.07@gmail.com>
2025-01-10 05:37:28 +0000

    Adding Login endpoint

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .models import LoginCode
 from .models import Profile
 from .models import VerificationCode
 
@@ -21,5 +22,11 @@ class ProfileAdmin(admin.ModelAdmin):
 
 @admin.register(VerificationCode)
 class VerificationCodeAdmin(admin.ModelAdmin):
+    list_display = ("user", "code", "created_at", "expired_at")
+    search_fields = ("user__name", "code")
+
+
+@admin.register(LoginCode)
+class LoginCodeAdmin(admin.ModelAdmin):
     list_display = ("user", "code", "created_at", "expired_at")
     search_fields = ("user__name", "code")
