8cc0ef8112d27003f5e33e6702d033f582e8d431
Arter Tendean <arter.tendean.07@gmail.com>
2025-01-15 23:03:02 +0700

    Update Social Media App (#46)
    * Adding Login endpoint
    
    * Finished posts list endpoint
    
    * Adding get post detail endpoint
    
    * Adding get post comment endpoint
    
    * Reset migrations

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
 from . import views
+from .views import PostCommentListView
+from .views import PostDetailView
+from .views import PostListView
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
+    path(
+        "posts/<str:post_slug>/comments/",
+        PostCommentListView.as_view(),
+        name="post-comments",
+    ),
+    path("", views.index, name="index"),
+]
 
 app_name = "social_media"
