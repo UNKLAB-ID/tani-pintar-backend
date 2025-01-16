f937ea40ad84cd8019f4d6fad4397d2f167afa81
Arter Tendean <arter.tendean.07@gmail.com>
2025-01-16 23:18:56 +0700

    Adding Refresh Token Endpoint (#50)
    * Adding Login endpoint
    
    * Adding refresh token endpoint
    
    * Clean the code

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
         views.ConfirmRegistrationView.as_view(),
         name="confirm-registration",
     ),
+    path("refresh-token/", views.RefreshTokenView.as_view(), name="refresh-token"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("login/confirm", views.ConfirmLoginView.as_view(), name="confirm-login"),
 ]
