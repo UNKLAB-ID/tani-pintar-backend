97cce8971da55e6f7984cfd261f0abea689b3014
Arter Tendean <arter.tendean.07@gmail.com>
2025-03-18 22:20:26 +0700

    Add serializers and view for PlantDisease analysis, enabling image uploads and response serialization

--- thinkflow/urls.py
+++ thinkflow/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
+from .views import PlantDiseaseAnalyzerView
 from .views import index
 
 urlpatterns = [
+    path(
+        "plant-disease/analyzer/",
+        PlantDiseaseAnalyzerView.as_view(),
+        name="plant-disease-analyzer",
+    ),
     path("", index, name="index"),
 ]
 
--- thinkflow/views.py
+++ thinkflow/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from thinkflow.serializers import CreatePlantDiseaseSerializer
+from thinkflow.serializers import GetPlantDiseaseSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the thinkflow index.")
+
+
+class PlantDiseaseAnalyzerView(GenericAPIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CreatePlantDiseaseSerializer
+        return super().get_serializer_class()
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        plant_disease = serializer.save()
+        plant_disease.analyze()
+
+        response_serializer = GetPlantDiseaseSerializer(plant_disease)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
