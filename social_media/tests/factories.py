0b2b298bb882f3d2a6abd97fcd51c2a714ab00f1
Arter Tendean <arter.tendean.07@gmail.com>
2025-07-29 23:06:27 +0700

    Add PostLike model tests and factory implementation

--- social_media/tests/factories.py
+++ social_media/tests/factories.py
@@ -5,6 +5,7 @@ from accounts.tests.factories import ProfileFactory
 from social_media.models import Post
 from social_media.models import PostComment
 from social_media.models import PostImage
+from social_media.models import PostLike
 
 
 class PostFactory(factory.django.DjangoModelFactory):
@@ -47,3 +48,13 @@ class PostCommentFactory(factory.django.DjangoModelFactory):
     )
     content = factory.Faker("paragraph", nb_sentences=3)
     parent = None
+
+
+class PostLikeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = PostLike
+
+    post = factory.SubFactory(PostFactory)
+    user = factory.LazyAttribute(
+        lambda _: ProfileFactory(profile_type=Profile.FARMER).user,
+    )
